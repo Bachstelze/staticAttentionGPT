cb3440ec0f6a9b928ffdbed75d797250051d3895
Wagtail <Bachstelze@users.noreply.github.com>
2024-05-19 15:16:50 +0200

    Update model.py
    comment the attention matrix definition

--- model.py
+++ model.py
@@ -32,7 +32,8 @@ class CausalSelfAttention(nn.Module):
         super().__init__()
         assert config.n_embd % config.n_head == 0
         # key, query, value projections for all heads, but in a batch
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        # they aren't used anymore
+        # self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
         # regularization
